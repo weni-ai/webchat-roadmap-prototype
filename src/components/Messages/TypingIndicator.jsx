343717eb96f5bb17b5e68ce590059a827edfdc82
Aldemylla <aldemylla@gmail.com>
2025-10-17 10:25:48 -0300

    refactor: Simplify TypingIndicator component by removing className prop and update styling for improved layout

--- src/components/Messages/TypingIndicator.jsx
+++ src/components/Messages/TypingIndicator.jsx
@@ -1,26 +1,19 @@
-import PropTypes from 'prop-types';
-
 import './TypingIndicator.scss';
 
 /**
  * TypingIndicator - Shows animated typing indicator when user is typing
  * Displays three animated dots to indicate that someone is typing
  */
-export function TypingIndicator({ className }) {
-
+export function TypingIndicator() {
   const CLASS_DOT = 'weni-typing-indicator__dot';
 
   return (
-    <section className={`weni-typing-indicator ${className || ''}`}>
+    <section className="weni-typing-indicator">
       <span className={CLASS_DOT} />
       <span className={CLASS_DOT} />
       <span className={CLASS_DOT} />
     </section>
   );
 }
-
-TypingIndicator.propTypes = {
-  className: PropTypes.string,
-};
 
 export default TypingIndicator;
